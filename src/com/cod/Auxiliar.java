c35c7e990306665f4219aab5a2a338ca2d10a821
Óscar Saá Pérez <osaaperez@danielcastelao.org>
2020-12-17 18:24:26 +0100

    Puse bien los corchetes, bn a mi manera.

--- src/com/cod/Auxiliar.java
+++ src/com/cod/Auxiliar.java
@@ -1,11 +1,30 @@
 package com.cod;
-public class Auxiliar {
-String e = "";
-String h = "127.0.0.2";
-Auxiliar(){
-e="anonymous@danielcastelao.org"; }
+public class Auxiliar
+{
+    String e = "e";
+    String h = "127.0.0.2";
+    Auxiliar()
+    {
+        e="anonymous@danielcastelao.org";
+    }
 
-Auxiliar(String param1){ this.e = param1; }
+    Auxiliar(String param1)
+    {
+        this.e = param1;
+    }
 
-public boolean con() {
-if (e!="anonymous@danielcastelao.org") { return true; } else {return false;}}}
+    public boolean con()
+    {
+    if (e!="anonymous@danielcastelao.org")
+    {
+        return true;
+    }
+        else
+            {
+                return false;
+            }
+    }
+
+
+
+}
