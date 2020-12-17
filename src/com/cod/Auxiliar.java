65d2721bd1897a8110d4c13873ca488760af77e4
Óscar Saá Pérez <osaaperez@danielcastelao.org>
2020-12-17 19:05:57 +0100

    trate de poerle singleton. falle

--- src/com/cod/Auxiliar.java
+++ src/com/cod/Auxiliar.java
@@ -3,15 +3,34 @@ public class Auxiliar
 {
     String e = "";
     String h = "127.0.0.2";
+
+    private static Auxiliar instance = null;
+
+    /**
+     * Constructor bloqueado con private
+     */
+
     Auxiliar()
+
     {
         e="anonymous@danielcastelao.org";
     }
-
     Auxiliar(String param1)
     {
         this.e = param1;
     }
+
+    public static Auxiliar getInstance() {
+
+        if (instance == null) {
+
+            instance = new Auxiliar();
+        }
+
+        return instance;
+    }
+
+
 
     public boolean con()
     {
